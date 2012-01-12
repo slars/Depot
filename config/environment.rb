b314ae9d43182ca1cb94ac819df10d5f9692f915
Stephen Larsen <stephen@slars.org>
2012-01-12 15:51:47 -0700

    Finished Chapter 13

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+# Initialize email capability
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:	    "smtp.gmail.com",
+    port:	    587,
+    domain:	    "slarsen.net",
+    authentication: "plain",
+    user_name:	    "bubbasteve",
+    password:	    "Gam3pl@n",
+    enable_starttls_auto: true
+  }
+end
